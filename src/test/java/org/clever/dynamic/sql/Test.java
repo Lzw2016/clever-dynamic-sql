c559a3bc1e442c9b2fafa3fb9df8a650652f0e00
lizw <lzw1000000@163.com>
2019-10-11 18:44:37 +0800

    init

---
+++ src/test/java/org/clever/dynamic/sql/Test.java
@@ -0,0 +1,16 @@
+package org.clever.dynamic.sql;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2019/10/11 18:43 <br/>
+ */
+@Slf4j
+public class Test {
+
+    @org.junit.Test
+    public void t1() {
+        log.info("------------------->");
+    }
+}
