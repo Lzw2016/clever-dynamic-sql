6e772ea3dd9c8f74a9fd301a6597b613410a5bea
lizw <lzw1000000@163.com>
2020-09-02 09:21:17 +0800

    重构 85%

--- src/main/java/org/clever/dynamic/sql/parsing/XNode.java
+++ src/main/java/org/clever/dynamic/sql/parsing/XNode.java
@@ -56,85 +56,11 @@ public class XNode {
     public XNode newXNode(Node node) {
         return new XNode(xpathParser, node, variables);
     }
-//
-//    public XNode getParent() {
-//        Node parent = node.getParentNode();
-//        if (!(parent instanceof Element)) {
-//            return null;
-//        } else {
-//            return new XNode(xpathParser, parent, variables);
-//        }
-//    }
 
-//    public String getPath() {
-//        StringBuilder builder = new StringBuilder();
-//        Node current = node;
-//        while (current instanceof Element) {
-//            if (current != node) {
-//                builder.insert(0, "/");
-//            }
-//            builder.insert(0, current.getNodeName());
-//            current = current.getParentNode();
-//        }
-//        return builder.toString();
-//    }
-
-//    public String getValueBasedIdentifier() {
-//        StringBuilder builder = new StringBuilder();
-//        XNode current = this;
-//        while (current != null) {
-//            if (current != this) {
-//                builder.insert(0, "_");
-//            }
-//            String value = current.getStringAttribute("id",
-//                    current.getStringAttribute("value",
-//                            current.getStringAttribute("property", null)));
-//            if (value != null) {
-//                value = value.replace('.', '_');
-//                builder.insert(0, "]");
-//                builder.insert(0,
-//                        value);
-//                builder.insert(0, "[");
-//            }
-//            builder.insert(0, current.getName());
-//            current = current.getParent();
-//        }
-//        return builder.toString();
-//    }
-
-    //    public String evalString(String expression) {
-//        return xpathParser.evalString(node, expression);
-//    }
-//
-//    public Boolean evalBoolean(String expression) {
-//        return xpathParser.evalBoolean(node, expression);
-//    }
-//
-//    public Double evalDouble(String expression) {
-//        return xpathParser.evalDouble(node, expression);
-//    }
-//
-//    public List<XNode> evalNodes(String expression) {
-//        return xpathParser.evalNodes(node, expression);
-//    }
-//
-//    public XNode evalNode(String expression) {
-//        return xpathParser.evalNode(node, expression);
-//    }
-//
     public Node getNode() {
         return node;
     }
 
-    //
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getStringBody() {
-//        return getStringBody(null);
-//    }
-//
     public String getStringBody(String def) {
         if (body == null) {
             return def;
@@ -142,79 +68,6 @@ public class XNode {
             return body;
         }
     }
-//
-//    public Boolean getBooleanBody() {
-//        return getBooleanBody(null);
-//    }
-//
-//    public Boolean getBooleanBody(Boolean def) {
-//        if (body == null) {
-//            return def;
-//        } else {
-//            return Boolean.valueOf(body);
-//        }
-//    }
-//
-//    public Integer getIntBody() {
-//        return getIntBody(null);
-//    }
-//
-//    public Integer getIntBody(Integer def) {
-//        if (body == null) {
-//            return def;
-//        } else {
-//            return Integer.parseInt(body);
-//        }
-//    }
-//
-//    public Long getLongBody() {
-//        return getLongBody(null);
-//    }
-//
-//    public Long getLongBody(Long def) {
-//        if (body == null) {
-//            return def;
-//        } else {
-//            return Long.parseLong(body);
-//        }
-//    }
-//
-//    public Double getDoubleBody() {
-//        return getDoubleBody(null);
-//    }
-//
-//    public Double getDoubleBody(Double def) {
-//        if (body == null) {
-//            return def;
-//        } else {
-//            return Double.parseDouble(body);
-//        }
-//    }
-//
-//    public Float getFloatBody() {
-//        return getFloatBody(null);
-//    }
-//
-//    public Float getFloatBody(Float def) {
-//        if (body == null) {
-//            return def;
-//        } else {
-//            return Float.parseFloat(body);
-//        }
-//    }
-//
-//    public <T extends Enum<T>> T getEnumAttribute(Class<T> enumType, String name) {
-//        return getEnumAttribute(enumType, name, null);
-//    }
-//
-//    public <T extends Enum<T>> T getEnumAttribute(Class<T> enumType, String name, T def) {
-//        String value = getStringAttribute(name);
-//        if (value == null) {
-//            return def;
-//        } else {
-//            return Enum.valueOf(enumType, value);
-//        }
-//    }
 
     public String getStringAttribute(String name) {
         return getStringAttribute(name, null);
@@ -228,71 +81,6 @@ public class XNode {
             return value;
         }
     }
-
-//    public Boolean getBooleanAttribute(String name) {
-//        return getBooleanAttribute(name, null);
-//    }
-//
-//    public Boolean getBooleanAttribute(String name, Boolean def) {
-//        String value = attributes.getProperty(name);
-//        if (value == null) {
-//            return def;
-//        } else {
-//            return Boolean.valueOf(value);
-//        }
-//    }
-//
-//    public Integer getIntAttribute(String name) {
-//        return getIntAttribute(name, null);
-//    }
-//
-//    public Integer getIntAttribute(String name, Integer def) {
-//        String value = attributes.getProperty(name);
-//        if (value == null) {
-//            return def;
-//        } else {
-//            return Integer.parseInt(value);
-//        }
-//    }
-//
-//    public Long getLongAttribute(String name) {
-//        return getLongAttribute(name, null);
-//    }
-//
-//    public Long getLongAttribute(String name, Long def) {
-//        String value = attributes.getProperty(name);
-//        if (value == null) {
-//            return def;
-//        } else {
-//            return Long.parseLong(value);
-//        }
-//    }
-//
-//    public Double getDoubleAttribute(String name) {
-//        return getDoubleAttribute(name, null);
-//    }
-//
-//    public Double getDoubleAttribute(String name, Double def) {
-//        String value = attributes.getProperty(name);
-//        if (value == null) {
-//            return def;
-//        } else {
-//            return Double.parseDouble(value);
-//        }
-//    }
-//
-//    public Float getFloatAttribute(String name) {
-//        return getFloatAttribute(name, null);
-//    }
-//
-//    public Float getFloatAttribute(String name, Float def) {
-//        String value = attributes.getProperty(name);
-//        if (value == null) {
-//            return def;
-//        } else {
-//            return Float.parseFloat(value);
-//        }
-//    }
 
     /**
      * 获取子节点
@@ -310,18 +98,6 @@ public class XNode {
         }
         return children;
     }
-
-//    public Properties getChildrenAsProperties() {
-//        Properties properties = new Properties();
-//        for (XNode child : getChildren()) {
-//            String name = child.getStringAttribute("name");
-//            String value = child.getStringAttribute("value");
-//            if (name != null && value != null) {
-//                properties.setProperty(name, value);
-//            }
-//        }
-//        return properties;
-//    }
 
     @Override
     public String toString() {
--- src/main/java/org/clever/dynamic/sql/parsing/XPathParser.java
+++ src/main/java/org/clever/dynamic/sql/parsing/XPathParser.java
@@ -11,6 +11,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
+import java.io.InputStream;
+import java.io.Reader;
 import java.io.StringReader;
 import java.util.Properties;
 
@@ -39,46 +41,6 @@ public class XPathParser {
      */
     private XPath xpath;
 
-//    public XPathParser(String xml) {
-//        commonConstructor(false, null, null);
-//        this.document = createDocument(new InputSource(new StringReader(xml)));
-//    }
-//
-//    public XPathParser(Reader reader) {
-//        commonConstructor(false, null, null);
-//        this.document = createDocument(new InputSource(reader));
-//    }
-//
-//    public XPathParser(InputStream inputStream) {
-//        commonConstructor(false, null, null);
-//        this.document = createDocument(new InputSource(inputStream));
-//    }
-//
-//    public XPathParser(Document document) {
-//        commonConstructor(false, null, null);
-//        this.document = document;
-//    }
-//
-//    public XPathParser(String xml, boolean validation) {
-//        commonConstructor(validation, null, null);
-//        this.document = createDocument(new InputSource(new StringReader(xml)));
-//    }
-//
-//    public XPathParser(Reader reader, boolean validation) {
-//        commonConstructor(validation, null, null);
-//        this.document = createDocument(new InputSource(reader));
-//    }
-//
-//    public XPathParser(InputStream inputStream, boolean validation) {
-//        commonConstructor(validation, null, null);
-//        this.document = createDocument(new InputSource(inputStream));
-//    }
-//
-//    public XPathParser(Document document, boolean validation) {
-//        commonConstructor(validation, null, null);
-//        this.document = document;
-//    }
-
     /**
      * @param xml        XML文本内容
      * @param validation 是否验证XML文档格式
@@ -89,115 +51,46 @@ public class XPathParser {
         this.document = createDocument(new InputSource(new StringReader(xml)));
     }
 
-//    public XPathParser(Reader reader, boolean validation, Properties variables) {
-//        commonConstructor(validation, variables, null);
-//        this.document = createDocument(new InputSource(reader));
-//    }
-//
-//    public XPathParser(InputStream inputStream, boolean validation, Properties variables) {
-//        commonConstructor(validation, variables, null);
-//        this.document = createDocument(new InputSource(inputStream));
-//    }
-//
-//    public XPathParser(Document document, boolean validation, Properties variables) {
-//        commonConstructor(validation, variables, null);
-//        this.document = document;
-//    }
+    public XPathParser(Reader reader, boolean validation, Properties variables) {
+        commonConstructor(validation, variables, null);
+        this.document = createDocument(new InputSource(reader));
+    }
+
+    public XPathParser(InputStream inputStream, boolean validation, Properties variables) {
+        commonConstructor(validation, variables, null);
+        this.document = createDocument(new InputSource(inputStream));
+    }
+
+    public XPathParser(Document document, boolean validation, Properties variables) {
+        commonConstructor(validation, variables, null);
+        this.document = document;
+    }
 
     public XPathParser(String xml, boolean validation, Properties variables, EntityResolver entityResolver) {
         commonConstructor(validation, variables, entityResolver);
         this.document = createDocument(new InputSource(new StringReader(xml)));
     }
 
-//    public XPathParser(Reader reader, boolean validation, Properties variables, EntityResolver entityResolver) {
-//        commonConstructor(validation, variables, entityResolver);
-//        this.document = createDocument(new InputSource(reader));
-//    }
-//
-//    public XPathParser(InputStream inputStream, boolean validation, Properties variables, EntityResolver entityResolver) {
-//        commonConstructor(validation, variables, entityResolver);
-//        this.document = createDocument(new InputSource(inputStream));
-//    }
-//
-//    public XPathParser(Document document, boolean validation, Properties variables, EntityResolver entityResolver) {
-//        commonConstructor(validation, variables, entityResolver);
-//        this.document = document;
-//    }
-//
-//    public void setVariables(Properties variables) {
-//        this.variables = variables;
-//    }
-//
-//    public String evalString(String expression) {
-//        return evalString(document, expression);
-//    }
+    public XPathParser(Reader reader, boolean validation, Properties variables, EntityResolver entityResolver) {
+        commonConstructor(validation, variables, entityResolver);
+        this.document = createDocument(new InputSource(reader));
+    }
+
+    public XPathParser(InputStream inputStream, boolean validation, Properties variables, EntityResolver entityResolver) {
+        commonConstructor(validation, variables, entityResolver);
+        this.document = createDocument(new InputSource(inputStream));
+    }
+
+    public XPathParser(Document document, boolean validation, Properties variables, EntityResolver entityResolver) {
+        commonConstructor(validation, variables, entityResolver);
+        this.document = document;
+    }
 
     public String evalString(Object root, String expression) {
         String result = (String) evaluate(expression, root, XPathConstants.STRING);
         result = PropertyParser.parse(result, variables);
         return result;
     }
-
-//    public Boolean evalBoolean(String expression) {
-//        return evalBoolean(document, expression);
-//    }
-//
-//    public Boolean evalBoolean(Object root, String expression) {
-//        return (Boolean) evaluate(expression, root, XPathConstants.BOOLEAN);
-//    }
-//
-//    public Short evalShort(String expression) {
-//        return evalShort(document, expression);
-//    }
-//
-//    public Short evalShort(Object root, String expression) {
-//        return Short.valueOf(evalString(root, expression));
-//    }
-//
-//    public Integer evalInteger(String expression) {
-//        return evalInteger(document, expression);
-//    }
-//
-//    public Integer evalInteger(Object root, String expression) {
-//        return Integer.valueOf(evalString(root, expression));
-//    }
-//
-//    public Long evalLong(String expression) {
-//        return evalLong(document, expression);
-//    }
-//
-//    public Long evalLong(Object root, String expression) {
-//        return Long.valueOf(evalString(root, expression));
-//    }
-//
-//    public Float evalFloat(String expression) {
-//        return evalFloat(document, expression);
-//    }
-//
-//    public Float evalFloat(Object root, String expression) {
-//        return Float.valueOf(evalString(root, expression));
-//    }
-//
-//    public Double evalDouble(String expression) {
-//        return evalDouble(document, expression);
-//    }
-//
-//    public Double evalDouble(Object root, String expression) {
-//        return (Double) evaluate(expression, root, XPathConstants.NUMBER);
-//    }
-//
-//    public List<XNode> evalNodes(String expression) {
-//        return evalNodes(document, expression);
-//    }
-//
-//    public List<XNode> evalNodes(Object root, String expression) {
-//        List<XNode> xnodes = new ArrayList<>();
-//        NodeList nodes = (NodeList) evaluate(expression, root, XPathConstants.NODESET);
-//        for (int i = 0; i < nodes.getLength(); i++) {
-//            xnodes.add(new XNode(this, nodes.item(i), variables));
-//        }
-//        return xnodes;
-//    }
 
     /**
      * 根据xpath表达式得到XML文档节点
